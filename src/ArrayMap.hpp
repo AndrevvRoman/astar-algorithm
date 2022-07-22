7e8ddd1f05958a664a38380bbfc1b760a8bfc79f
Andrey Romanov <andrevvroman@gmail.com>
2022-07-22 11:24:51 +0700

    Add useage of enum into getPoint

--- src/ArrayMap.hpp
+++ src/ArrayMap.hpp
@@ -1,11 +1,15 @@
 #pragma once
+#include <iostream>
 
 class ArrayMap
 {
 public:
     static constexpr int MAP_WIDTH = 20;
     static constexpr int MAP_HEIGHT = 20;
-    int logical[MAP_HEIGHT][MAP_WIDTH] =
+
+    // Instance of actual map that will be used in search. Nobody is allowed
+    // to change this variable.
+    const int logical[MAP_HEIGHT][MAP_WIDTH] =
         {
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
             {1, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 1},
@@ -28,6 +32,8 @@ public:
             {1, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9, 1, 1, 1, 1, 1, 1, 1, 1},
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
 
+    // Copy of logical map that used for marking down path, visited nodes and all stuff
+    // that not involved in process of search, but can be displayed to user in some form
     int frontend[MAP_HEIGHT][MAP_WIDTH] =
         {
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
@@ -62,23 +68,27 @@ public:
         CLOSE_PATH_POS = 104,
     };
     // map helper functions
-    int getPoint(int x, int y) const
+    CellType getPoint(int x, int y) const
     {
-        if (x < 0 ||
-            x >= MAP_WIDTH ||
-            y < 0 ||
-            y >= MAP_HEIGHT)
+        if (x < 0 || x >= MAP_WIDTH ||
+            y < 0 || y >= MAP_HEIGHT)
         {
-            return 9;
+            return CellType::WALL_POS;
         }
 
-        return logical[y][x];
+        return static_cast<CellType>(logical[y][x]);
     }
 
     void setCell(const int x, const int y, CellType mark)
     {
         frontend[y][x] = static_cast<int>(mark);
     }
+
+    // Singletone used as MapSearchNode depends oh this object
+    // and we can expect that AStarSearch will constantly construct MapSearchNode instances.
+    // So to avoid messing with pointer or bunch of copies of ArrayMap I used singletone
+    // to be able access to this class from everywhere without constructing another instance
+public:
     static ArrayMap &getInstance()
     {
         static ArrayMap map;
@@ -86,5 +96,8 @@ public:
     }
 
 private:
-    ArrayMap(){};
+    ArrayMap()
+    {
+        std::cout << "Construct singletone !!!" << std::endl;
+    };
 };
